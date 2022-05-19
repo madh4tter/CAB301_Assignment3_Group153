14422637deed5dd4fceb7e238591a6c65ca52a43
Nuttion <flynn.kassulke@bigpond.com>
2022-05-20 09:30:24 +1000

    Project Setup

---
+++ CAB301 Assignment 3 Group 153/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAB301_Assignment_3_Group_153
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
